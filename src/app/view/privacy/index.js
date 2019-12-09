339b46b4c0a1b430b0330e7c65759782daaa0c23
Sourav Badami <souravbadami@gmail.com>
2019-12-09 16:16:15 +0530

    Updates legal information.

--- src/app/view/privacy/index.js
+++ src/app/view/privacy/index.js
@@ -40,7 +40,20 @@ export default class Privacy extends React.Component {
                     <div className="row">
                         <div className="col-md-12">
                             <Jumbotron>
-                                
+                            <h2><strong>Your Privacy</strong></h2>
+
+<p>Please read Privacy Policy</p>
+
+<h3><strong>Reservation of Rights</strong></h3>
+
+<p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
+
+<h3><strong>Removal of links from our website</strong></h3>
+
+<p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.</p>
+
+<p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.</p>
+
                             </Jumbotron>
                         </div>
                     </div>
