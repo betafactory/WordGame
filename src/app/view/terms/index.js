6886c7cc268b31916403e972860d7de4d5e7467c
Sourav Badami <souravbadami@gmail.com>
2019-12-10 00:29:44 +0530

    Updates terms.

--- src/app/view/terms/index.js
+++ src/app/view/terms/index.js
@@ -46,7 +46,7 @@ export default class Terms extends React.Component {
 
 <p>These terms and conditions outline the rules and regulations for the use of Beta Factory's Website, located at http://wordgame.betafactory.tech/.</p>
 
-<p>By accessing this website we assume you accept these terms and conditions. Do not continue to use Word Game if you do not agree to take all of the terms and conditions stated on this page. Our Terms and Conditions were created with the help of the <a href="https://www.termsandconditionsgenerator.com">Terms And Conditions Generator</a> and the <a href="https://www.termsconditionsgenerator.com">Terms & Conditions Generator</a>.</p>
+<p>By accessing this website we assume you accept these terms and conditions. Do not continue to use Word Game if you do not agree to take all of the terms and conditions stated on this page.</p>
 
 <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
 
