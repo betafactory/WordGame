6d2ccbe039e98cdd2f8b0b0e8b28e6df0933ba52
Sourav Badami <souravbadami@gmail.com>
2019-12-18 13:07:49 +0530

    Displays graph on a daily basis and adds amateur field to progress tracker.

--- src/app/view/profile/index.js
+++ src/app/view/profile/index.js
@@ -112,7 +112,27 @@ export default class Profile extends React.Component {
                                        pointRadius: 1,
                                        pointHitRadius: 10,
                                        data: data["progress"]["overall"]["baseline"]["data"]
-                                   },
+                                   },{
+                                        label: 'Amateur',
+                                        fill: false,
+                                        lineTension: 0.1,
+                                        backgroundColor: 'rgba(75,192,192,0.4)',
+                                        borderColor: 'rgba(255,192,0,1)',
+                                        borderCapStyle: 'butt',
+                                        borderDash: [5, 5],
+                                        borderDashOffset: 0.0,
+                                        borderJoinStyle: 'miter',
+                                        pointBorderColor: 'rgba(75,192,192,1)',
+                                        pointBackgroundColor: '#fff',
+                                        pointBorderWidth: 1,
+                                        pointHoverRadius: 5,
+                                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                                        pointHoverBorderColor: 'rgba(220,220,220,1)',
+                                        pointHoverBorderWidth: 2,
+                                        pointRadius: 1,
+                                        pointHitRadius: 10,
+                                        data: data["progress"]["overall"]["amateur"]["data"]
+                                    },
                                    {
                                        label: 'Novice',
                                        fill: false,
